6018f9bcef09ed64249a33e62986dac88fb50993
Fornoth <fornoth01@gmail.com>
2015-03-28 07:34:00 +0000

    Show not logged in message instead of hiding player, don't set metadata when its empty, add album cover placeholder, hide some buttons when not logged in

--- static/js/main.js
+++ static/js/main.js
@@ -52,6 +52,19 @@ function updateMetadata() {
         var artist = $('#artistInfo');
         var album = $('#albumInfo');
         var albumCover = $('#albumCover');
+        var musicInfo = $('[data-music-info]');
+        var albumCoverPlaceholder = $('#albumCoverPlaceholder');
+
+        //Temporary fix until better error checking is added server side
+        if (metadata.track_uri === '') {
+            musicInfo.text('No music playing');
+            albumCover.hide();
+            albumCoverPlaceholder.show();
+            return;
+        }
+
+        albumCover.show();
+        albumCoverPlaceholder.hide();
 
         track.attr('data-id', metadata.track_uri);
         track.text(metadata.track_name);
@@ -70,6 +83,10 @@ function updateMetadata() {
 
 function getStatus() {
     $.ajax('/api/info/status').done(function(data) {
+        var musicInfo = $('[data-music-info]');
+        var albumCover = $('#albumCover');
+        var albumCoverPlaceholder = $('#albumCoverPlaceholder');
+
         //Display buttons depending on play state
         $('[data-action=play]').toggle(!data.playing);
         $('[data-action=pause]').toggle(data.playing);
@@ -77,7 +94,7 @@ function getStatus() {
         $('[data-action=shuffle]').toggleClass('active', data.shuffle);
         $('[data-action=repeat]').toggleClass('active', data.repeat);
 
-        $('#player').toggle(data.logged_in);
+        //$('#player').toggle(data.logged_in);
 
         $('#activeDevice').text(data.active);
         $('#controls button').toggleClass('disabled', !data.active);
@@ -86,11 +103,18 @@ function getStatus() {
         $('#logoutLink').toggle(data.logged_in);
 
         if (!loggedIn && data.logged_in && !metadataSetup) {
+            $('[data-login-required]').show();
+            albumCover.show();
+            albumCoverPlaceholder.hide();
             loggedIn = true;
             metadataSetup = true;
             updateMetadata();
             metadataInterval = setInterval(updateMetadata, 5000);
         } else if (!data.logged_in) {
+            $('[data-login-required]').hide();
+            musicInfo.text('Not logged in');
+            albumCover.hide();
+            albumCoverPlaceholder.show();
             loggedIn = false;
             metadataSetup = false;
             clearInterval(metadataInterval);
